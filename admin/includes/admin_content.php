c9515e4c151d4e315d0dbd57a8a405c62b62ff5c
LeonMarkes <leonm@DESKTOP-RP404ON>
2019-02-03 17:45:37 +0100

    Created instantiation and has_the_attribute methods.

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -16,8 +16,11 @@
                     echo $row['username'] . "<br>";
                 }
 
-                //$found_user = User::find_user_by_id(2);
-                //echo $found_user['username'];
+                // $found_user = User::find_user_by_id(2);
+
+                // $user = User::instantiation($found_user);
+
+                // echo $user->first_name;
 
                 ?>
 
